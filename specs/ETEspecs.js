260a256e4e1ea7de0c1f4f55d337c8215fa70f77
Foad Mozaffari <foadmozaffari@gmail.com>
2015-04-03 20:42:30 -0700

    refactor homepage acceptance test.

--- specs/ETEspecs.js
+++ specs/ETEspecs.js
@@ -1,28 +1,20 @@
 //Home page
-var AppHomepage = function(){
-    this.assert = ['first-text', 'first-user', 'first-date',
-        'second-text', 'second-user', 'second-date'];
-    var assertions = [];
-    this.assert.forEach(function(entry){
-        var testClass = '.' + entry;
-        assertions.push(element(by.css(testClass)));
+var Homepage = function(){
+    browser.get('http://localhost:8080');
+    this.tweets = element.all(by.css(".tweets li")).map(function(li) {
+       return li.$$(".text").map(function(div) {
+           return div.getText();
+       });
     });
-    this.assertions = assertions;
-    this.get = function(){
-        browser.get('http://localhost:8080/#/');
-    }
 }
-describe('app home page', function(){
-    it('should check to see if tweet texts exist', function(){
-        var appHomepage = new AppHomepage();
-        var matchedData = ['Four more years. http://t.co/bAJE6Vom','Foad Mozaffari','Mon Sep 24 03:35:21 +0000 2012',
-            'maruthe cat maruthe cat maruthe cat','Chaz Martenstein','Mon Sep 21 04:35:21 +0000 2012'];
-        appHomepage.get();
-        var i = 0;
-        appHomepage.assertions.forEach(function(entry){
-            expect((entry).getText()).toEqual(matchedData[i]);
-            i = i + 1;
-        });
+
+describe('app home page', function() {
+    it('displays tweets', function() {
+        homepage = new Homepage();
+        expect(homepage.tweets).toEqual([
+            ['Four more years. http://t.co/bAJE6Vom', 'Foad Mozaffari',   'Mon Sep 24 03:35:21 +0000 2012'],
+            ['maruthe cat maruthe cat maruthe cat',   'Chaz Martenstein', 'Mon Sep 21 04:35:21 +0000 2012']
+        ]);
     })
 })
 
@@ -46,6 +38,5 @@ describe('app detail page', function(){
         expect((appDetailpage.detailUser).getText()).toEqual('Foad Mozaffari');
         expect((appDetailpage.detailDate).getText()).toEqual('Mon Sep 24 03:35:21 +0000 2012');
         expect((appDetailpage.detailHash).getText()).toEqual('#grumpyCat');
-
     })
 })
